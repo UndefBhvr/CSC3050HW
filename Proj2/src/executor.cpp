fa5fc2444c2a7c6b21cfd4919a6c0a2cfcfa9926
ILSYT <1756304680@qq.com>
2023-11-01 07:57:19 +0800

    Add file IO

--- Proj2/src/executor.cpp
+++ Proj2/src/executor.cpp
@@ -12,7 +12,7 @@ void execute()
     xldz::sp = xldz::STACK_BEGIN;
     xldz::pc = xldz::TEXT_BEGIN;
     xldz::gp = xldz::DATA_BEGIN + 0x8000;
-    u32 cnt=0;
+    u32 cnt = 0;
     while (1)
     {
         snapshot(cnt++);
